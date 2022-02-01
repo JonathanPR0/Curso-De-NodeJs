7f02bae3232536a1115a8890425f13014e35f326
Jon-PR0 <jonathan.almeida1793@gmail.com>
2022-01-31 21:16:23 -0300

    Express - Parte 1

---
+++ 6_EXPRESS/3-middleware/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const basePath = path.join(__dirname, 'templates');
+
+const checkAuth = (req, res, next) => {
+  req.authStatus = true;
+
+  if (req.authStatus) {
+    console.log('Está logado, pode continuar');
+    next();
+  } else {
+    console.log('Não está logado, faça o login para continuar');
+    next();
+  }
+};
+
+app.use(checkAuth);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(basePath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
